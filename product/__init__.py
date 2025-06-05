57692c1e99fce04779aeb4f36a9aa438d39af77c
NeelShah-NS <nss10009@nyu.edu>
2025-06-05 19:20:11 -0400

    IBOR Future Valuation

--- product/__init__.py
+++ product/__init__.py
@@ -1,3 +1,3 @@
 from .product import (LongOrShort, Product)
-from .linear_products import (ProductBulletCashflow, ProductFuture)
-from .product_display_visitor import (CashflowVisitor, FutureVisitor)
+from .linear_products import (ProductBulletCashflow, ProductFuture, ProductIborCashflow, ProductOvernightCashflow, ProductRfrFuture, ProductIborSwap, ProductOvernightSwap)
+from .product_display_visitor import (CashflowVisitor, FutureVisitor, IborCashflowVisitor, OvernightCashflowVisitor, RfrFutureVisitor, IborSwapVisitor, OvernightSwapVisitor)
--- yield_curve/valuation_engine_yc.py
+++ yield_curve/valuation_engine_yc.py
@@ -1,10 +1,11 @@
-from .yield_curve_model import YiedCurve
-from product import (LongOrShort, ProductBulletCashflow, ProductFuture)
+from .yield_curve_model import YieldCurve
+from product import (LongOrShort, ProductBulletCashflow, ProductFuture, ProductRfrFuture)
 from valuation import (ValuationEngine)
+from date import TermOrTerminationDate, Date
 
 class ValuationEngineProductBulletCashflow(ValuationEngine):
 
-    def __init__(self, model : YiedCurve, valuationParameters : dict, product : ProductBulletCashflow):
+    def __init__(self, model : YieldCurve, valuationParameters : dict, product : ProductBulletCashflow):
         super().__init__(model, valuationParameters, product)
         self.currency = product.currency
         self.maturity = product.terminationDate
@@ -16,4 +17,23 @@ class ValuationEngineProductBulletCashflow(ValuationEngine):
         discountFactor = self.model.discountFactor(self.fundingIndex, self.maturity)
         self.value_ = [self.currency.value.code(), self.notional * self.buyOrSell * discountFactor]
 
-### TODO: Implement val engine for future/swap (libor/rfr) etc
+### TODO: Implement val engine for swap (libor/rfr) etc
+
+class ValuationEngineProductFuture(ValuationEngine):
+
+    def __init__(self, model: YieldCurve, valuationParameters: dict, product: ProductFuture):
+        super().__init__(model, valuationParameters, product)
+        self.currency      = product.currency
+        self.effectiveDate = product.effectiveDate
+        self.indexKey      = product.index
+        self.strike        = product.strike
+        self.buyOrSell     = 1.0 if product.longOrShort.value == LongOrShort.LONG else -1.0
+        self.notional      = product.notional
+
+    def calculateValue(self):
+        forwardRate = self.model.forward(self.indexKey, self.effectiveDate)
+        #Futures are quoted as Price = 1 − forwardRate
+        futuresPrice = 1.0 - forwardRate
+        pnl = (futuresPrice - self.strike) * self.notional * self.buyOrSell
+        self.value_ = [ self.currency.value.code(), pnl ]
+
